31d685cef7deee611ddfc36829998ddfe6e85c56
bkrastev95 <bobusa036@gmail.com>
2016-05-13 23:50:41 +0300

    Driver mappings

--- KAT.Data.Services/DriverService.cs
+++ KAT.Data.Services/DriverService.cs
@@ -6,26 +6,57 @@ using AutoMapper;
 using KAT.Data.IServices;
 using KAT.Data.KAT.Context;
 using KAT.Data.Services.Utilities;
+using KAT.Web.Models;
 using Driver = KAT.Web.Models.Driver;
 
 namespace KAT.Data.Services
 {
     public class DriverService : IDriverService
     {
+        private readonly IMapper mapper;
+
         public DriverService()
         {
-            Mapper.CreateMap<CodeFirstModels.Driver, Driver>();
-            Mapper.CreateMap<Driver, CodeFirstModels.Driver>();
+            /*
+             var config = new MapperConfiguration(cfg => {
+            cfg.CreateMap<Order, OrderDto>()
+                  .Include<OnlineOrder, OnlineOrderDto>()
+            cfg.CreateMap<OnlineOrder, OnlineOrderDto>();
+            });
+             */
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<CodeFirstModels.Driver, Driver>()
+                    .IncludeBase<CodeFirstModels.Driver, SimpleDriver>();
+
+                cfg.CreateMap<Driver, CodeFirstModels.Driver>()
+                    .IncludeBase<SimpleDriver, CodeFirstModels.Driver>();
+
+                cfg.CreateMap<Car, CodeFirstModels.Car>();
+                cfg.CreateMap<CodeFirstModels.Car, Car>();
+
+                cfg.CreateMissingTypeMaps = true;
+            });
+
+            mapper = config.CreateMapper();
         }
 
         public List<Driver> GetAllDrivers()
         {
-            var drivers = new List<Driver>();
-            using (var context = new KatDataContext())
+            try
             {
-                var result = context.Drivers.ToList();
-                result.ForEach(r => drivers.Add(Mapper.Map<Driver>(r)));
-                return drivers;
+                var drivers = new List<Driver>();
+                using (var context = new KatDataContext())
+                {
+                    var result = context.Drivers.ToList();
+                    result.ForEach(r => drivers.Add(mapper.Map<Driver>(r)));
+                    return drivers;
+                }
+            }
+            catch (Exception e)
+            {
+                return new List<Driver>();
             }
         }
 
